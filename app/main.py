ca968037e06d3cebe80fe7cbde1dcdad5a630f74
srohitreddy <srohitreddy5@gmail.com>
2026-02-26 12:47:59 +0530

    space

---
+++ app/main.py
@@ -0,0 +1,16 @@
+from fastapi import FastAPI
+from app.api.v1.router import api_router
+from app.core.database import Base, engine
+
+
+# Create database tables
+Base.metadata.create_all(bind=engine)
+
+# FastAPI instance (must be global)
+app = FastAPI(title="Student CRUD API")
+
+@app.get("/")
+def root():
+    return {"message": "Student CRUD API is running "}
+# Include API routes
+app.include_router(api_router, prefix="/api/v1")
