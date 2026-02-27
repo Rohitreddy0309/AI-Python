ceef4c9a57826180aa35890ca3c568c32f66e291
srohitreddy <srohitreddy5@gmail.com>
2026-02-27 15:16:35 +0530

    remove password from database

--- core/database.py
+++ core/database.py
@@ -1,21 +1,25 @@
 from sqlalchemy import create_engine
-from sqlalchemy.orm import declarative_base, sessionmaker
+from sqlalchemy.orm import sessionmaker, declarative_base
 
-DATABASE_URL: str = "postgresql://postgres:postgres@localhost:5432/CURD_database"
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+DATABASE_URL = os.getenv("DATABASE_URL")
 
 engine = create_engine(DATABASE_URL)
 
 SessionLocal = sessionmaker(
     autocommit=False,
     autoflush=False,
-    bind=engine,
+    bind=engine
 )
 
 Base = declarative_base()
 
 
 def get_db():
-    """Provide database session."""
     db = SessionLocal()
     try:
         yield db
